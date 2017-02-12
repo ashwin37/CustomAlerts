ffeb580145b9c3894283410c3ae4d93334381029
ashwin37 <ashwint379@gmail.com>
2017-02-12 01:48:53 -0500

    Custom Alert using PCLBlurEffectAlert, Generating a preview !!

--- CustomAlerts/ViewController.swift
+++ CustomAlerts/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import PCLBlurEffectAlert
 
 class ViewController: UIViewController {
 
@@ -15,11 +16,18 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func AlertActnBtn(_ sender: Any)
+    {
+        
+        let alert = PCLBlurEffectAlert.Controller(title: "Show Me the Meaning of Lonely !!", message: "Something else...!!", effect: UIBlurEffect(style: .dark), style: .actionSheet)
+        let alertBtn = PCLBlurEffectAlert.AlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(alertBtn)
+        
+        alert.configure(cornerRadius:10)
+        alert.configure(overlayBackgroundColor: UIColor(red: 10/255, green: 40/255, blue: 60/255, alpha: 0.6))
+        alert.configure(titleFont:UIFont.systemFont(ofSize: 30), titleColor:UIColor.blue)
+        alert.configure(messageFont:UIFont.systemFont(ofSize: 30), messageColor: UIColor.black)
+        alert.show()
     }
-
-
 }
 
